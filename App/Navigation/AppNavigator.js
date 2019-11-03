ff8fc709c031a625ac09933bf7ae152f1d12bedc
Carlos Rosario <carg1975@hotmail.com>
2019-11-03 19:13:38 -0400

    Implementar SwitchNavigator con nuevo AuthNavigator y MainTabNavigator

--- App/Navigation/AppNavigator.js
+++ App/Navigation/AppNavigator.js
@@ -1,11 +1,13 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
+import AuthNavigator from './AuthNavigator';
 import MainTabNavigator from './MainTabNavigator';
 
 export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+    Auth: AuthNavigator,
     Main: MainTabNavigator,
   })
 );

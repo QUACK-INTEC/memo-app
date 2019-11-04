1ce3e29b63358858302150462abca36a192ba7c3
francinelucca <francinelucca@hotmail.com>
2019-11-04 12:06:24 -0400

    Component: InLineComponent

---
+++ App/Components/Common/InLineComponent/InLineComponent.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, View, ViewPropTypes } from 'react-native';
+import PropTypes from 'prop-types';
+
+class InLineComponent extends React.Component {
+  renderInLineComponent = () => {
+    const { leftChild, rightChild, viewStyle } = this.props;
+    return (
+      <View style={[styles.viewStyle, viewStyle]}>
+        {leftChild()}
+        {rightChild()}
+      </View>
+    );
+  };
+
+  render() {
+    return this.renderInLineComponent();
+  }
+}
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
+
+InLineComponent.defaultProps = {
+  leftChild: () => null,
+  rightChild: () => null,
+  viewStyle: null,
+};
+
+InLineComponent.propTypes = {
+  leftChild: PropTypes.func,
+  rightChild: PropTypes.func,
+  viewStyle: ViewPropTypes.style,
+};
+
+export default InLineComponent;

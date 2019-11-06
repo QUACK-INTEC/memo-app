1c49d0dd63b2cbd466a18ee30ac0102ccc6a7a7e
Víctor Manuel Díaz Mateo <victordiazmateo@gmail.com>
2019-11-06 13:16:46 -0400

    Created Link component.

---
+++ App/Components/Common/Link/Link.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, ViewPropTypes } from 'react-native';
+import PropTypes from 'prop-types';
+
+// Theme
+import { colors, fonts } from '../../../Core/Theme';
+
+// Common
+import Text from '../Text';
+
+class Link extends React.Component {
+  handleOnPress = () => {
+    const { onPress, disabled } = this.props;
+
+    if (!disabled) {
+      return onPress();
+    }
+
+    return null;
+  };
+
+  render() {
+    const { text, textStyle } = this.props;
+    return (
+      <TouchableOpacity onPress={this.handleOnPress}>
+        <Text.Black style={[styles.textStyle, textStyle]} text={text} />
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  textStyle: { textDecorationLine: 'underline', color: colors.GRAY, ...fonts.SIZE_XS },
+});
+
+Link.defaultProps = {
+  text: null,
+  textStyle: null,
+  disabled: null,
+  onPress: () => null,
+};
+
+Link.propTypes = {
+  text: null,
+  textStyle: ViewPropTypes.style,
+  disabled: null,
+  onPress: PropTypes.func,
+};
+
+export default Link;

76df5388fac37eb3452c0a5617408f5adaf5158b
Víctor Manuel Díaz Mateo <victordiazmateo@gmail.com>
2019-11-10 19:47:35 -0400

    Finished link component.

--- App/Components/Common/Link/Link.js
+++ App/Components/Common/Link/Link.js
@@ -3,7 +3,7 @@ import { StyleSheet, TouchableOpacity, ViewPropTypes } from 'react-native';
 import PropTypes from 'prop-types';
 
 // Theme
-import { colors, fonts } from '../../../Core/Theme';
+import { colors, fonts, toBaseDesignPx } from '../../../Core/Theme';
 
 // Common
 import ImageWrapper from '../ImageWrapper';
@@ -21,25 +21,25 @@ class Link extends React.Component {
     return null;
   };
 
-  renderLeftIcon() {
+  renderLeftIcon = () => {
     const { leftMemoSrc, leftUri } = this.props;
 
     if (leftMemoSrc || leftUri) {
-      return <ImageWrapper memoSrc={leftMemoSrc} uri={leftUri} />;
+      return <ImageWrapper memoSrc={leftMemoSrc} uri={leftUri} style={styles.badgeStyle} />;
     }
 
     return null;
-  }
+  };
 
-  renderRightIcon() {
+  renderRightIcon = () => {
     const { rightMemoSrc, rightUri } = this.props;
 
     if (rightMemoSrc || rightUri) {
-      return <ImageWrapper memoSrc={rightMemoSrc} uri={rightUri} />;
+      return <ImageWrapper memoSrc={rightMemoSrc} uri={rightUri} style={styles.badgeStyle} />;
     }
 
     return null;
-  }
+  };
 
   render() {
     const { text, textStyle } = this.props;
@@ -55,6 +55,11 @@ class Link extends React.Component {
 
 const styles = StyleSheet.create({
   textStyle: { textDecorationLine: 'underline', color: colors.GRAY, ...fonts.SIZE_XS },
+  badgeStyle: {
+    width: toBaseDesignPx(11.5),
+    height: toBaseDesignPx(11.5),
+    justifyContent: 'flex-end',
+  },
 });
 
 Link.defaultProps = {

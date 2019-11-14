22389db7f7e84895847e2d849598e86557c252d1
Víctor Manuel Díaz Mateo <victordiazmateo@gmail.com>
2019-11-14 01:42:14 -0400

    Finished radioButton.

--- App/Components/Common/RadioButton/RadioButton.js
+++ App/Components/Common/RadioButton/RadioButton.js
@@ -1,37 +1,70 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity, ViewPropTypes } from 'react-native';
+import { StyleSheet } from 'react-native';
+import RadioButton from 'react-native-radio-button';
 import PropTypes from 'prop-types';
 
 // Theme
-import { colors, fonts } from '../../../Core/Theme';
+import { colors, spacers } from '../../../Core/Theme';
 
 // Common
-import Text from '../Text';
 import InLineComponent from '../InLineComponent';
+import Text from '../Text';
 
-class RadioButton extends React.Component {
-  render = () => {
-    const { text, textStyle } = this.props;
-    return (
-      <TouchableOpacity onPress={this.handleOnPress}>
-        <InLineComponent leftChild={this.renderLeftIcon} rightChild={this.renderRightIcon}>
-          <Text.Black style={[styles.textStyle, textStyle]} text={text} />
-        </InLineComponent>
-      </TouchableOpacity>
-    );
+class RadioButtonWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPressed: props.isPressed,
+    };
+  }
+
+  handlePress = isPressed => {
+    const { onPress } = this.props;
+    this.setState({ isPressed });
+    onPress(isPressed);
   };
+
+  renderText = () => {
+    const { text, textStyle } = this.props;
+
+    if (text) {
+      return <Text.SemiBold text={text} style={[styles.textStyle, textStyle]} />;
+    }
+
+    return null;
+  };
+
+  render() {
+    const { isPressed } = this.state;
+    return (
+      <InLineComponent>
+        <RadioButton
+          innerColor={isPressed ? colors.ORANGE_LIGHT : colors.WHITE}
+          outerColor={isPressed ? colors.ORANGE : colors.GRAY}
+          isSelected={isPressed}
+          onPress={() => this.handlePress(!isPressed)}
+        />
+        {this.renderText()}
+      </InLineComponent>
+    );
+  }
 }
 const styles = StyleSheet.create({
-  textStyle: { color: colors.GRAY, ...fonts.SIZE_XS },
+  textStyle: { color: colors.GRAY, ...spacers.ML_2 },
 });
 
-RadioButton.defaultProps = {
+RadioButtonWrapper.defaultProps = {
+  text: null,
   textStyle: null,
+  isPressed: false,
+  onPress: () => null,
 };
 
-RadioButton.propTypes = {
-  text: PropTypes.string.isRequired,
-  textStyle: ViewPropTypes.style,
+RadioButtonWrapper.propTypes = {
+  text: PropTypes.string,
+  textStyle: PropTypes.shape({}),
+  isPressed: PropTypes.bool,
+  onPress: PropTypes.func,
 };
 
-export default RadioButton;
+export default RadioButtonWrapper;

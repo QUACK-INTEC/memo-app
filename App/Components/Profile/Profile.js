fe8238a3aad6f96f3230a4a9dba066894f2b2410
Victor Diaz <victordiazmateo@gmail.com>
2019-11-24 00:22:04 -0400

    Started working with profile screen.

---
+++ App/Components/Profile/Profile.js
@@ -0,0 +1,18 @@
+// import React, { Component } from 'react';
+// import { View, StyleSheet, SafeAreaView } from 'react-native';
+
+// import Button from '../Common/Button';
+// import Text from '../Common/Text';
+// import Section from '../Common/Section';
+// import Avatar from '../Common/Avatar';
+// import Icon, { ICON_TYPE, ICON_SIZE } from '../Common/Icon';
+
+// class Profile extends React.Component {
+//   render() {
+//     return
+//     (
+//     <View>
+
+//     </View>);
+//   }
+// }

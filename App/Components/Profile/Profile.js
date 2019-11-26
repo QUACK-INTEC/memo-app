d36fc6960d25bea91dbb984828578f16aed521e5
Víctor Manuel Díaz Mateo <victordiazmateo@gmail.com>
2019-11-25 23:43:21 -0400

    Finished profile screen.

---
+++ App/Components/Profile/Profile.js
@@ -0,0 +1,163 @@
+import React from 'react';
+import { View, StyleSheet, SafeAreaView } from 'react-native';
+import PropTypes from 'prop-types';
+
+import Avatar from '../Common/Avatar';
+import Text from '../Common/Text';
+import Icon, { ICON_TYPE, ICON_SIZE } from '../Common/Icon';
+import InLineComponent from '../Common/InLineComponent';
+import Section from '../Common/Section';
+import ImageWrapper from '../Common/ImageWrapper';
+
+import { fonts, spacers, constants, colors, toBaseDesignPx } from '../../Core/Theme';
+
+class Profile extends React.Component {
+  renderEmail = () => {
+    const { studentMail } = this.props;
+    return <Text.SemiBold text={studentMail} />;
+  };
+
+  renderSubjects = () => {
+    const { studentSubjects } = this.props;
+    return <Text.SemiBold text={studentSubjects} />;
+  };
+
+  renderEditIcon = () => {
+    return (
+      <View style={styles.editIconContainer}>
+        <Icon
+          name="edit"
+          type={ICON_TYPE.MEMO_ICONS}
+          size={ICON_SIZE.TINY}
+          onPress={this.showEditOptions}
+          color={colors.GRAY}
+        />
+      </View>
+    );
+  };
+
+  renderHeader = () => {
+    return (
+      <InLineComponent viewStyle={styles.header}>
+        <View style={styles.headerBackIconContainer}>
+          <Icon
+            name="chevron-circle-left"
+            type={ICON_TYPE.FONT_AWESOME}
+            size={ICON_SIZE.TINY}
+            color={colors.TRANSPARENT}
+            onPress={this.handleBackArrow}
+          />
+        </View>
+        {this.renderEditIcon()}
+      </InLineComponent>
+    );
+  };
+
+  render() {
+    const {
+      avatarSrc,
+      avatarUri,
+      avatarInitialsText,
+      badgeSrc,
+      badgeUri,
+      memoPoints,
+      rank,
+      studentName,
+    } = this.props;
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={[styles.headerInfoContainer]}>
+          {this.renderHeader()}
+          <Avatar
+            initialsText={avatarInitialsText}
+            src={avatarSrc}
+            uri={avatarUri}
+            style={styles.avatar}
+          />
+          <InLineComponent viewStyle={styles.studentSection}>
+            <Text.Medium text={studentName} style={styles.studentName} />
+            <ImageWrapper memoSrc={badgeSrc} uri={badgeUri} style={styles.badgeStyle} />
+          </InLineComponent>
+          <InLineComponent viewStyle={styles.memoPointsRow}>
+            <Text.SemiBold text="MP:" style={styles.memoPointsLabel} />
+            <Text.SemiBold text={`${memoPoints} ~ ${rank}`} style={styles.memoPointsValue} />
+          </InLineComponent>
+          <Section title="Clases" viewStyle={styles.sectionViewStyle}>
+            {this.renderSubjects()}
+          </Section>
+          <Section title="Mail" viewStyle={styles.sectionViewStyle}>
+            {this.renderEmail()}
+          </Section>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+Profile.defaultProps = {
+  studentSubjects: null,
+  avatarSrc: null,
+  avatarUri: null,
+  avatarInitialsText: null,
+  badgeSrc: null,
+  badgeUri: null,
+};
+
+Profile.propTypes = {
+  studentName: PropTypes.string.isRequired,
+  studentMail: PropTypes.string.isRequired,
+  studentSubjects: PropTypes.string,
+  avatarSrc: PropTypes.string,
+  avatarUri: PropTypes.string,
+  avatarInitialsText: PropTypes.string,
+  badgeSrc: PropTypes.string,
+  badgeUri: PropTypes.string,
+  memoPoints: PropTypes.string.isRequired,
+  rank: PropTypes.string.isRequired,
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  headerInfoContainer: { marginTop: constants.DEVICE.STATUS_BAR_HEIGHT },
+  avatar: { alignSelf: 'center', justifyContent: 'center' },
+  studentName: {
+    ...fonts.SIZE_XL,
+    textAlign: 'center',
+  },
+  memoPointsRow: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    ...spacers.MT_9,
+  },
+  memoPointsValue: { color: colors.GRAY, ...spacers.ML_2, ...fonts.SIZE_XS },
+  memoPointsLabel: { color: colors.GRAY_LIGHT, ...fonts.SIZE_XS },
+  sectionViewStyle: { ...spacers.MB_8 },
+  editIconContainer: {
+    ...spacers.MR_14,
+    ...spacers.MB_4,
+    width: toBaseDesignPx(47),
+    justifyContent: 'flex-end',
+  },
+  header: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerBackIconContainer: {
+    ...spacers.ML_14,
+    ...spacers.MB_4,
+    width: toBaseDesignPx(47),
+    justifyContent: 'flex-start',
+  },
+  studentSection: {
+    ...spacers.MT_3,
+    alignSelf: 'center',
+  },
+  badgeStyle: {
+    width: toBaseDesignPx(12),
+    height: toBaseDesignPx(12),
+    alignSelf: 'center',
+    ...spacers.ML_2,
+  },
+});
+
+export default Profile;
--- App/Screens/Profile/Profile.js
+++ App/Screens/Profile/Profile.js
@@ -1,16 +1,93 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
+import LoadingState from '../../Components/LoadingState';
 
-import Text from '../../Components/Common/Text';
+import ProfileComponent from '../../Components/Profile';
 
-const Profile = () => (
-  <View style={styles.container}>
-    <Text.Bold text="PROFILE" />
-  </View>
-);
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      studentName: null,
+      studentMail: null,
+      studentSubjects: null,
+      avatarUri: null,
+      avatarSrc: null,
+      avatarInitialsText: null,
+      badgeUri: null,
+      badgeSrc: null,
+      memoPoints: null,
+      rank: null,
+    };
+  }
+
+  componentDidMount() {
+    const {
+      navigation: { getParam },
+    } = this.props;
+    const studentName = getParam('studentName', {});
+    const studentMail = getParam('studentMail', {});
+    const studentSubjects = getParam('studentSubjects', {});
+    const avatarUri = getParam('avatarUri', {});
+    const avatarSrc = getParam('avatarSrc', {});
+    const avatarInitialsText = getParam('avatarInitialsText', {});
+    const badgeUri = getParam('badgeUri', {});
+    const badgeSrc = getParam('badgeSrc', {});
+    const memoPoints = getParam('memoPoints', {});
+    const rank = getParam('rank', {});
+
+    this.setState({
+      isLoading: false,
+      studentName,
+      studentMail,
+      studentSubjects,
+      avatarUri,
+      avatarSrc,
+      avatarInitialsText,
+      badgeUri,
+      badgeSrc,
+      memoPoints,
+      rank,
+    });
+  }
+
+  render() {
+    const {
+      isLoading,
+      studentName,
+      studentMail,
+      studentSubjects,
+      avatarUri,
+      avatarSrc,
+      avatarInitialsText,
+      badgeUri,
+      badgeSrc,
+      memoPoints,
+      rank,
+    } = this.state;
+    return (
+      <View style={styles.container}>
+        <LoadingState.Modal isLoading={isLoading} />
+        <ProfileComponent
+          studentName={studentName}
+          studentMail={studentMail}
+          studentSubjects={studentSubjects}
+          avatarUri={avatarUri}
+          avatarSrc={avatarSrc}
+          avatarInitialsText={avatarInitialsText}
+          badgeUri={badgeUri}
+          badgeSrc={badgeSrc}
+          memoPoints={memoPoints}
+          rank={rank}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  container: { flex: 1 },
 });
 
 export default Profile;

c8d68e71cc89b41376a1df605e86a705757ba908
Jonathan Taveras <jonathanrtv@outlook.com>
2019-11-26 08:10:53 -0400

    [WIP] Settings item

---
+++ App/Components/SettingsItem/SettingsItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+// TODO: Create settings item to be able to handle different childrens like the design
+class SettingsItem extends React.Component {
+  render() {
+    return <View></View>;
+  }
+}
+
+export default SettingsItem;

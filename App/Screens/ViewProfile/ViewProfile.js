b33e43036220451b321baf38a0b759cc1c0d8a5e
Víctor Manuel Díaz Mateo <victordiazmateo@gmail.com>
2019-11-27 11:02:28 -0400

    Started viewProfile screen.

--- App/Screens/ViewProfile/ViewProfile.js
+++ App/Screens/ViewProfile/ViewProfile.js
@@ -1,21 +1,122 @@
 import React from 'react';
-import { View, StyleSheet, Alert } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
+import PropTypes from 'prop-types';
+import LoadingState from '../../Components/LoadingState';
 
-import Text from '../../Components/Common/Text';
+import ProfileComponent from '../../Components/Profile';
+import ClassInfoCard from '../../Components/ClassInfoCard';
+import ClassesComponent from '../../Components/Classes';
+
+import { Classes } from '../../Models';
+
+import { spacers } from '../../Core/Theme';
 
 class ViewProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      studentName: null,
+      studentMail: null,
+      studentSubjects: null,
+      avatarUri: null,
+      avatarSrc: null,
+      avatarInitialsText: null,
+      badgeUri: null,
+      badgeSrc: null,
+      memoPoints: null,
+      rank: null,
+    };
+  }
+
   componentDidMount() {
     const {
       navigation: { getParam },
     } = this.props;
-    const userId = getParam('userId', {});
-    Alert.alert(`${userId}`);
+    const studentName = getParam('studentName', {});
+    const studentMail = getParam('studentMail', {});
+    const studentSubjects = getParam('studentSubjects', {});
+    const avatarUri = getParam('avatarUri', {});
+    const avatarSrc = getParam('avatarSrc', {});
+    const avatarInitialsText = getParam('avatarInitialsText', {});
+    const badgeUri = getParam('badgeUri', {});
+    const badgeSrc = getParam('badgeSrc', {});
+    const memoPoints = getParam('memoPoints', {});
+    const rank = getParam('rank', {});
+
+    this.setState({
+      isLoading: false,
+      studentName,
+      studentMail,
+      studentSubjects,
+      avatarUri,
+      avatarSrc,
+      avatarInitialsText,
+      badgeUri,
+      badgeSrc,
+      memoPoints,
+      rank,
+    });
   }
 
+  renderSubject = ({ item }) => {
+    return (
+      <View style={styles.myClassContainer}>
+        <ClassInfoCard
+          subject={item.subjectName}
+          professor={item.professorName}
+          schedule={item.classDays}
+          onPress={() => this.handleOnPressClassItem(item.id, item)}
+        />
+      </View>
+    );
+  };
+
+  renderClasses = () => {
+    const { commonClasses, commonClassesLookup } = this.props;
+    const myClassesFormatted = Classes.getClassesData(commonClasses, commonClassesLookup);
+
+    return (
+      <FlatList
+        columnWrapperStyle={styles.classesContainer}
+        data={myClassesFormatted}
+        numColumns={2}
+        renderItem={this.renderSubject}
+        keyExtractor={item => item.id}
+      />
+    );
+  };
+
   render() {
+    const {
+      isLoading,
+      studentName,
+      studentMail,
+      studentSubjects,
+      avatarUri,
+      avatarSrc,
+      avatarInitialsText,
+      badgeUri,
+      badgeSrc,
+      memoPoints,
+      rank,
+    } = this.state;
     return (
       <View style={styles.container}>
-        <Text.Bold text="USER PROFILE" />
+        <LoadingState.Modal isLoading={isLoading} />
+        <ProfileComponent
+          studentName={studentName}
+          studentMail={studentMail}
+          studentSubjects={studentSubjects}
+          avatarUri={avatarUri}
+          avatarSrc={avatarSrc}
+          avatarInitialsText={avatarInitialsText}
+          badgeUri={badgeUri}
+          badgeSrc={badgeSrc}
+          memoPoints={memoPoints}
+          rank={rank}
+        />
+        <ClassesComponent renderClasses={this.renderClasses} />
       </View>
     );
   }
@@ -23,6 +124,18 @@ class ViewProfile extends React.Component {
 
 const styles = StyleSheet.create({
   container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  classesContainer: { justifyContent: 'space-between', flex: 1 },
+  myClassContainer: { ...spacers.MA_1 },
 });
+
+ViewProfile.defaultProps = {
+  commonClasses: [],
+  commonClassesLookup: {},
+};
+
+ViewProfile.propTypes = {
+  commonClasses: PropTypes.arrayOf(PropTypes.string),
+  commonClassesLookup: PropTypes.shape({}),
+};
 
 export default ViewProfile;

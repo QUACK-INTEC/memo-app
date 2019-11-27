e3efe9e2c6bd8c989b865038b8f7d8e1bd9f471d
Carlos Rosario <carg1975@hotmail.com>
2019-11-27 10:54:40 -0400

    Implementacion de Refresh Token

---
+++ App/Core/Assets/Fonts/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable global-require */
+const FontList = {
+  'poppins-black': require('./Poppins-Black.ttf'),
+  'poppins-black-italic': require('./Poppins-BlackItalic.ttf'),
+  'poppins-bold': require('./Poppins-Bold.ttf'),
+  'poppins-bold-italic': require('./Poppins-BoldItalic.ttf'),
+  'poppins-extra-bold': require('./Poppins-ExtraBold.ttf'),
+  'poppins-extra-bold-italic': require('./Poppins-ExtraBoldItalic.ttf'),
+  'poppins-extra-light': require('./Poppins-ExtraLight.ttf'),
+  'poppins-extra-light-italic': require('./Poppins-ExtraLightItalic.ttf'),
+  'poppins-italic': require('./Poppins-Italic.ttf'),
+  'poppins-light': require('./Poppins-Light.ttf'),
+  'poppins-light-italic': require('./Poppins-LightItalic.ttf'),
+  'poppins-medium': require('./Poppins-Medium.ttf'),
+  'poppins-medium-italic': require('./Poppins-MediumItalic.ttf'),
+  'poppins-regular': require('./Poppins-Regular.ttf'),
+  'poppins-semi-bold': require('./Poppins-SemiBold.ttf'),
+  'poppins-semi-bold-italic': require('./Poppins-SemiBoldItalic.ttf'),
+  'poppins-thin': require('./Poppins-Thin.ttf'),
+  'poppins-thin-italic': require('./Poppins-ThinItalic.ttf'),
+};
+
+export default FontList;
